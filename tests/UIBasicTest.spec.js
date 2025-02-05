218bbcc3fb99fbc49cc8f9f67b3caab523d710e5
Nisarg <nisujaiswal4@gmail.com>
2025-02-05 15:48:00 +0530

    login with incorrect and correect creds test added

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -1,19 +1,52 @@
 import { expect, test } from "@playwright/test";
 
-test("Using Browser Method", async ({ browser }) => {
-  const context = await browser.newContext();
-  const page = await context.newPage();
+// Testing using Browser method (Long)
+// test("Using Browser Method", async ({ browser }) => {
+//   const context = await browser.newContext();
+//   const page = await context.newPage();
 
-  await page.goto("https://google.com");
+//   await page.goto("https://google.com");
 
-  console.log(await page.title());
+//   console.log(await page.title());
 
-  await expect(page).toHaveTitle("Google");
+//   await expect(page).toHaveTitle("Google");
+// });
+
+// test("Using Default Parameter", async ({ browser, page }) => {
+//   await page.goto("https://www.youtube.com/");
+
+//   console.log(await page.title());
+//   await expect(page).toHaveTitle("YouTube");
+// });
+
+// Interacting with inputs:
+
+test("Logging in with wrong credentials", async ({ page }) => {
+  const errorMsg = page.locator("[style*='none']");
+  const username = page.locator("input#username");
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  await username.fill("Nisu Jaiswal");
+  await page.locator("[name = 'password']").fill("learning");
+  await page.locator("[type='submit']").click();
+
+  console.log(await errorMsg.textContent());
+
+  expect(errorMsg).toContainText("Incorrect");
 });
 
-test("Using Default Parameter", async ({ browser, page }) => {
-  await page.goto("https://www.youtube.com/");
+// Logging in with right credentials
+test("Logging in with correct credentials", async ({ page }) => {
+  const username = page.locator("input#username");
 
-  console.log(await page.title());
-  await expect(page).toHaveTitle("YouTube");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+  await username.fill("rahulshettyacademy");
+  await page.locator("[name = 'password']").fill("learning");
+  await page.locator("[type='submit']").click();
+
+  const firstElement = page.locator(".card-title").first();
+
+  console.log(await firstElement.textContent());
 });
